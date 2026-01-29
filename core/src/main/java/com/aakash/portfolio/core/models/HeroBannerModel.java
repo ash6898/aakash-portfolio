fc040465ad4c7b148a12fe9a2af74e6ed5568aa8
Aakash Baskaran <aakashguru6898@gmail.com>
2026-01-29 14:25:09 -0500

    refactor(hero-banner): replace hardcoded content  with JCR content

---
+++ core/src/main/java/com/aakash/portfolio/core/models/HeroBannerModel.java
@@ -0,0 +1,66 @@
+package com.aakash.portfolio.core.models;
+
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.injectorspecific.SlingObject;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
+@Model(adaptables = { Resource.class,
+        SlingHttpServletRequest.class }, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+public class HeroBannerModel {
+
+    @SlingObject
+    private Resource resource;
+
+    @ValueMapValue
+    private String jobTitle;
+
+    @ValueMapValue
+    private String name;
+
+    @ValueMapValue
+    private String description;
+
+    @ValueMapValue
+    private String button1Text;
+
+    @ValueMapValue
+    private String button2Text;
+
+    @ValueMapValue
+    private String button1Path;
+
+    @ValueMapValue
+    private String button2Path;
+
+    // Getters
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getButton1Text() {
+        return button1Text;
+    }
+
+    public String getButton2Text() {
+        return button2Text;
+    }
+
+    public String getButton1Path() {
+        return button1Path;
+    }
+
+    public String getButton2Path() {
+        return button2Path;
+    }
+}
